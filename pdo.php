1a70d3daa80489113d457442a90aeec7ffbef18b
Ismael Castaneda Villegas <ismaelcastaneda.v@gmail.com>
2022-07-19 20:16:06 -0500

    Revising database connection

--- pdo.php
+++ pdo.php
@@ -1,8 +1,8 @@
 <?php
     require_once "credentials.php";
 
-    $pdo = new PDO("mysql:host=sql3.freesqldatabase.com;port=3306;dbname=$DB_NAME",
-        $DB_NAME, $DB_PWD);
+    $pdo = new PDO("mysql:host=sql3.freesqldatabase.com;port=3306;dbname=sql3505436",
+        'sql3505436', 'p9YaPAJQk9');
 
     //throws error if select if wrong
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
